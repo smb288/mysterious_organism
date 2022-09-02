bd3fa518ca9f6cee85c62f3c480c2616480d3625
Sean Baker <sbaker101@gmail.com>
2022-09-02 13:09:34 -0500

    Created factory function for DNA strands

--- main.js
+++ main.js
@@ -14,9 +14,32 @@ const mockUpStrand = () => {
 };
 
 
+// Factory function that holds a generated DNA
+// strand and a number for each specimen
+function pAequorFactory(specimenNumber, dna) {
+  return {
+    specimenNumber: specimenNumber,
+    dna: dna,
+    // Selects random DNA base and regenerates 
+    mutate() {
+      let dnaIndex = Math.floor(Math.random() * dna.length);
+      let baseArray = ['A','T','C','G'];
+      let newBase = '';
 
+      do {
+        let randomBase = Math.floor(Math.random() * 4);
+        newBase = baseArray[randomBase];
+      } while(newBase === this.dna[dnaIndex]);
 
+      this.dna[dnaIndex] = newBase;
+    }
+  };
+}
 
+let aequorArray = []
+for (let i = 0; i < 5; i++)
+  aequorArray.push(pAequorFactory(i, mockUpStrand()))
 
-
-
+console.log(aequorArray[3].dna);
+aequorArray[3].mutate();
+console.log(aequorArray[3].dna);
